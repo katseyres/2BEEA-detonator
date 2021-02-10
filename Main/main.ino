3a548f31e886cf5fb38ba224b19fd5121df3fe76
Maximilien Denis <denismax98@hotmail.fr>
2021-02-10 23:08:11 +0100

    correct mistakes

--- Main/main.ino
+++ Main/main.ino
@@ -11,8 +11,8 @@ const char kp4x4Keys[ROWS][COLS] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-byte KEYPAD_4x4_rowPin[ROWS] = {9, 8, 7, 6};
-byte KEYPAD_4x4_colPin[COLS] = {5, 4, 3, 2};
+byte KEYPAD_4x4_rowPin[ROWS] = {22, 23, 24, 25};
+byte KEYPAD_4x4_colPin[COLS] = {26, 27, 28, 29};
 
 Keypad kp4x4 = Keypad(makeKeymap(kp4x4Keys),KEYPAD_4x4_rowPin, KEYPAD_4x4_colPin,ROWS, COLS);
 
@@ -21,7 +21,7 @@ char userInput[PASSWORD_LENGTH];
 char password[PASSWORD_LENGTH] = {'5', '1', '7', '0'};
 bool isPasswordTrue;
 
-LiquidCrystal lcd(4, 6, 11, 12, 13, 14);
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 /* ---------------------------------------------------------------- */
 
@@ -39,22 +39,8 @@ void setup()
 
 void loop()
 {
-  bool password = KEYPAD_4x4_ReadInput();
-
-  if (digitalRead(7) == HIGH)
-  {
-    if (password)
-    {
-      CD4511_StartCounter();
-    }
-  }
-  else
-  {
-    lcd.clear();
-    lcd.print("TURN ON THE SWITCH");
-  }
-
-    delay(10);
+  KEYPAD_4x4_ReadInput();
+  delay(10);
 }
 
 /* ---------------------------------------------------------------- */
@@ -62,47 +48,63 @@ void loop()
 bool KEYPAD_4x4_ReadInput()
 {
   char customKey = kp4x4.getKey();
-
-  if(customKey)
+  
+  if (!isPasswordTrue)
   {
-    lcd.print("*");
-    userInput[counter] = customKey;
-    counter++;
-  }
 
-  if(counter == PASSWORD_LENGTH)
-  {
-    lcd.clear();
-    isPasswordTrue = true;
-    counter = 0;
-
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    if(customKey)
     {
-      if (userInput[i] != password[i])
+      Serial.print("*");
+      lcd.print("*");
+      userInput[counter] = customKey;
+      counter++;
+    }
+
+    if(counter == PASSWORD_LENGTH)
+    {
+      lcd.clear();
+      isPasswordTrue = true;
+      counter = 0;
+
+      for (int i = 0; i < PASSWORD_LENGTH; i++)
       {
-        isPasswordTrue = false;
-        break;
+        if (userInput[i] != password[i])
+        {
+          isPasswordTrue = false;
+          break;
+        }
       }
-    }
 
-    if (isPasswordTrue)
-    {
-      lcd.print("PASSWORD CORRECT");
-      delay(1000);
-      lcd.clear();
-      lcd.print("ARMED !");
-      return true;
-    }
-    else
-    {
-      lcd.print("INCORRECT");
-      delay(1000);
-      lcd.clear();
-      lcd.print("PASSWORD : ");
-      return false;
-    }
+      if (isPasswordTrue)
+      {
+        lcd.clear();
+        lcd.print("PASSWORD CORRECT");
+        delay(2000);
+        while (digitalRead(7) != HIGH)
+        {
+          lcd.clear();
+          lcd.print("TURN ON THE");
+          lcd.setCursor(0,1);
+          lcd.print("SWITCH");
+          delay(10);
+        }
+        lcd.clear();
+        lcd.print("ARMED !");
+        return true;
+      }
+      else
+      {
+        lcd.clear();
+        lcd.print("INCORRECT");
+        delay(1000);
+        lcd.clear();
+        lcd.print("PASSWORD : ");
+        return false;
+      }
 
-    char userInput[4];
+      char userInput[4];
+    }
+    return false;
   }
 }
 
@@ -134,7 +136,7 @@ void CD4511_SetPins(){
 
   //-----------------------
 
-  pinMode(7, INPUT); // Switch in INPUT mode
+  pinMode(7, OUTPUT); // Switch in INPUT mode
 
 }
 
